c293c6cac252d1ca04237747ba22a81c187ed6ca
GraphICL0 <156929564+GraphICL0@users.noreply.github.com>
2024-01-18 15:08:35 +0800

    Create citeseer.sh

---
+++ sh/citeseer.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES_DEFAULT=0
+
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES_DEFAULT python main.py --embedder graphicl --propagate True --dataset Citeseer --setting lt --imbalance_ratio 100
